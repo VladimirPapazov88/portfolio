ff83631e0c0b35743af58ac981255d55567fd550
Vladimir Papazov <papazovvladimir88@gmail.com>
2024-03-25 20:43:43 +0300

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -158,7 +158,7 @@ const Card = ({
   );
 };
  
-export const Icon = ({ className, ...rest }: any) => {
+const Icon = ({ className, ...rest }: any) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
